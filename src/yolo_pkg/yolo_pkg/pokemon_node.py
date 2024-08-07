99fba12dad0fcfaf458f35c0fa739c3f0d137336
ocar1053 <64206644+ocar1053@users.noreply.github.com>
2024-08-07 17:16:50 +0800

    Update pokemon_node.py
    setting relative path

--- src/yolo_pkg/yolo_pkg/pokemon_node.py
+++ src/yolo_pkg/yolo_pkg/pokemon_node.py
@@ -4,6 +4,8 @@ from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from ultralytics import YOLO
 import cv2
+import os
+
 
 class PokemonNode(Node):
 
@@ -11,16 +13,20 @@ class PokemonNode(Node):
         super().__init__('pokemon_yolo_node')
         self.get_logger().info('Pokemon Node is running')
         self.bridge = CvBridge()
-        self.model = YOLO("/home/ocar1053/ros2_ws/src/yolo_pkg/resource/best.pt")
+        self.current_dir = os.path.dirname(os.path.realpath(__file__))
+        self.model_path = os.path.join(self.current_dir, 'resource', 'best.pt')
+        self.model = YOLO(self.model_path)
         self.subscription = self.create_subscription(
-                Image,
-                'camera_topic',
-                self.listener_callback,
-                10)
+            Image,
+            'camera_topic',
+            self.listener_callback,
+            10)
+
     def listener_callback(self, msg):
         self.get_logger().info('Received frame')
         frame = self.bridge.imgmsg_to_cv2(msg, 'bgr8')
-        results = self.model.track(frame, persist=True)  # convert to track mode
+        results = self.model.track(
+            frame, persist=True)  # convert to track mode
 
         # visualizing results
         annotated_frame = results[0].plot()
@@ -32,11 +38,14 @@ class PokemonNode(Node):
         if cv2.waitKey(1) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             rclpy.shutdown()
+
+
 def main():
     rclpy.init()
     node = PokemonNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
+
 if __name__ == '__main__':
-    main()    
+    main()
